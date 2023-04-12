fd0d2281b3ce5b59dfb03414e9abd964705c9a7c
Pravin Bhagwat <85389806+pravinbhagwat@users.noreply.github.com>
2023-04-12 11:58:04 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,16 +1,16 @@
 //complete this code
 class Person {
 	constructor(name, age) {
-		this._name = name;
-		this._age = age;
+		this.name = name;
+		this.age = age;
 	}
 
 	get getName() {
-		return this._name;
+		return this.name;
 	}
 
 	set setAge(age) {
-		this._age = age;
+		this.age = age;
 	}
 }
 
